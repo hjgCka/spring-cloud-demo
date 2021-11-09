3547f5405bde82dbc3487baad602aeb9438c001c
hjg <computer_cka@163.com>
2021-11-09 22:09:45 +0800

    使用更多的eureka配置。

--- eureka-client-invoker/src/main/java/com/hjg/cloud/controller/InvokerController.java
+++ eureka-client-invoker/src/main/java/com/hjg/cloud/controller/InvokerController.java
@@ -1,9 +1,16 @@
 package com.hjg.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @description:
@@ -16,6 +23,9 @@ public class InvokerController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     /**
      * 使用http://localhost:8082/router?name=James来访问，可以得到UserController的结果。
      * @param name
@@ -24,5 +34,34 @@ public class InvokerController {
     @RequestMapping(value = "/router")
     public String router(String name) {
         return restTemplate.getForObject("http://provider/hello?name="+name, String.class);
+    }
+
+    /**
+     * 使用DiscoveryClient客户端。
+     * @return
+     */
+    @RequestMapping(value = "/meta-info")
+    public Map<String, String> metaData() {
+        Map<String, String> result = new HashMap<>();
+
+        List<String> list = discoveryClient.getServices();
+        result.put("serviceList", list.stream().collect(Collectors.joining("_")));
+
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("provider");
+        for(ServiceInstance instance : instanceList) {
+
+            result.put("instanceId", instance.getInstanceId());
+            result.put("host", instance.getHost());
+            result.put("port", instance.getPort()+"");
+            result.put("serviceId", instance.getServiceId());
+
+            Map<String, String> metaMap = instance.getMetadata();
+            String keys = metaMap.keySet().stream().collect(Collectors.joining("_"));
+            result.put("keys", keys);
+            String values = metaMap.values().stream().collect(Collectors.joining("_"));
+            result.put("values", values);
+        }
+
+        return result;
     }
 }
